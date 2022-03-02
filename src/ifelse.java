85ac350bf79eeded34f0ac41e7c1cc5b2de5510a
MPkarthikeyan1411 <100142286+MPkarthikeyan1411@users.noreply.github.com>
2022-03-02 11:02:46 +0530

    if else

---
+++ src/ifelse.java
@@ -0,0 +1,19 @@
+public class ifelse {
+
+        public static void main(String args[]){
+            int n=0;
+            if (n==0){
+                System.out.println("NOTHING");
+                System.out.println("BYE");
+            }
+            else
+            if (n%2==0)
+                System.out.println("EVEN");
+            else
+                System.out.println("ODD");
+
+        }
+
+    }
+
+
