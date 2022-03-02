37fea243e25f084d9b4937ace90bcc2a142b9878
MPkarthikeyan1411 <100142286+MPkarthikeyan1411@users.noreply.github.com>
2022-03-02 10:43:08 +0530

    boom

---
+++ src/bur.java
@@ -0,0 +1,26 @@
+class calc7
+{
+    int num1;
+    int num2;
+    int result;
+    public calc7()
+    {
+        num1 =5;
+        num2=5;
+    }
+
+}
+
+
+
+public class bur {
+
+    public static void main(String args[]){
+        calc7 obj = new calc7();
+
+        System.out.println(obj.num1);
+
+    }
+
+}
+
